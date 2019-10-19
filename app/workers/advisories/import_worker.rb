e764dbcde67a1a1ee55bca9f5483115f10342745
Serhii Budnik <serhiibudnik@gmail.com>
2019-10-20 00:41:32 +0300

    added  import worker

---
+++ app/workers/advisories/import_worker.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Advisories
+  class ImportWorker
+    include Sidekiq::Worker
+
+    def perform
+      Advisories::Import.new.call do |outcome|
+        outcome.success do |result|
+          p result
+        end
+
+        outcome.failure do |message|
+          raise message
+        end
+      end
+    end
+  end
+end
