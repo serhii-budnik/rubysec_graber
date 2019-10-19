1445454e64ffc18d7372963619f86df5269ca412
Serhii Budnik <serhiibudnik@gmail.com>
2019-10-19 19:08:48 +0300

    added additional fields to advisories table

--- db/migrate/20191015222845_create_advisories.rb
+++ db/migrate/20191015222845_create_advisories.rb
@@ -1,13 +1,19 @@
 class CreateAdvisories < ActiveRecord::Migration[6.0]
   def change
     create_table :advisories do |t|
+      enable_extension 'hstore'
+
       t.string :identifier, unique: true, index: true
       t.string :gem
       t.string :cve
+      t.string :osvdb
+      t.string :cvss_v2
+      t.string :cvss_v3
       t.string :url
       t.string :title
       t.date :date
       t.text :description
+      t.hstore :related, :default => {}
       t.text :unaffected_versions, array: true, default: []
       t.text :patched_versions, array: true, default: []
       t.timestamps null: false
