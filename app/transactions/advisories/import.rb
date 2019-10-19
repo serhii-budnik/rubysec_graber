07d17d4aa77ba445eb51804c680f5d31aaa56ffc
Serhii Budnik <serhiibudnik@gmail.com>
2019-10-20 00:41:02 +0300

    uncomment line

--- app/transactions/advisories/import.rb
+++ app/transactions/advisories/import.rb
@@ -5,6 +5,6 @@ module Advisories
     include Dry::Transaction(container: Container)
 
     step :rubysec_import, with: 'advisories.rubysec_import'
-    # step :new_advisories_notification, with: 'users.new_advisories_notification'
+    step :new_advisories_notification, with: 'users.new_advisories_notification'
   end
 end
