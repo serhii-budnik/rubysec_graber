effef7331de4ab108017153c65cc906b2b9ef123
Serhii Budnik <serhiibudnik@gmail.com>
2019-10-19 19:09:38 +0300

    load lib dir and dry-transaction

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
-require "dry/container"
-require "dry/transaction"
-require "dry/transaction/operation"
+require 'dry/container'
+require 'dry/transaction'
+require 'dry/transaction/operation'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,6 +14,8 @@ module RubysecGraber
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.autoload_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
