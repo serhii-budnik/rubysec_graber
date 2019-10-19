399a036a7aa3991e2b54c432e2563fb5a8af2e28
Serhii Budnik <serhiibudnik@gmail.com>
2019-10-20 01:46:54 +0300

    added confirmed scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  scope :confirmed, -> { where.not(confirmed_at: nil) }
 end
