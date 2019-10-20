2442840191e941d1849cc7c81657504061cb2874
Serhii Budnik <serhiibudnik@gmail.com>
2019-10-20 18:53:28 +0300

    added const for advisory members

--- app/adapters/advisory_adapter.rb
+++ app/adapters/advisory_adapter.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class AdvisoryAdapter < Struct
+  MEMBERS = %i[filepath gem cve osvdb url title date description
+               cvss_v2 cvss_v3 patched_versions unaffected_versions
+               related].freeze
+
   def initialize(hash)
     super(hash.slice(*members))
   end
