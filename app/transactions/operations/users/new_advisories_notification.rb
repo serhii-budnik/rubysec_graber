970d1f90d2944060863b4d16ca3339b1330635ef
Serhii Budnik <serhiibudnik@gmail.com>
2019-10-21 03:19:48 +0300

    added telegram notification

--- app/transactions/operations/users/new_advisories_notification.rb
+++ app/transactions/operations/users/new_advisories_notification.rb
@@ -7,7 +7,11 @@ module Operations::Users
     def call(input)
       @advisories = input
 
+      return Success([]) if advisories.blank?
+
       notify_users_via_emails
+      notify_users_via_telegram
+
       Success(advisories)
     end
 
@@ -17,6 +21,18 @@ module Operations::Users
 
     def notify_users_via_emails
       AdvisoryMailer.new_advisories(advisories.map(&:id)).deliver_later
+    end
+
+    def notify_users_via_telegram
+      Telegram.bot.send_message(
+        chat_id: TELEGRAM_CHAT_ID, text: telegram_message, parse_mode: 'HTML'
+      )
+    end
+
+    def telegram_message
+      advisories.map do |ad|
+        "#{ad.gem} - <a href=\"#{ad.url}\">#{ad.title}</a>"
+      end.join("\n")
     end
   end
 end
--- spec/transactions/operations/users/new_advisories_notification_spec.rb
+++ spec/transactions/operations/users/new_advisories_notification_spec.rb
@@ -7,6 +7,14 @@ describe Operations::Users::NewAdvisoriesNotification, '#call' do
 
   context 'when going to notify users about an advisory' do
     it 'should send email' do
+      stub_request(:post, 'https://api.telegram.org/bot/sendMessage').to_return(
+        body: %({"ok":true, "result":{"message_id":13,"author_signature":"RubysecBot",
+                "chat":{"id":-1001301260856, "title":"Rubysec Advisories",
+                "username":"rubysec_advisories", "type":"channel"},
+                "date":1571616860, "text":"test message"}}),
+        status: 200
+      )
+
       Sidekiq::Testing.inline! do
         described_class.new.call([advisory])
 
